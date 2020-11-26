323450f6093c64ba9c22c5384d529d5e2ac8b5bc
Nico-rl <nico.xtream@live.cl>
2020-11-26 07:48:25 -0300

    bd

---
+++ app/src/main/java/cl/santotomas/formulario/ConexionSQLiteHelper.java
@@ -0,0 +1,26 @@
+package cl.santotomas.formulario;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class ConexionSQLiteHelper extends SQLiteOpenHelper {
+
+    final String CREAR_TABLA_USUARIO="CREATE TABLE usuarios (id INTEGER, nombre TEXT, apellido TEXT, numeroTarjeta INTEGER, mesTarjeta TEXT, anioTarjeta INTEGER, codigoTarjeta INTEGER, calleNumero TEXT, ciudad TEXT, estado TEXT, codigoPostal INTEGER) ";
+
+    public ConexionSQLiteHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREAR_TABLA_USUARIO);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int versionAntigua, int versionNueva) {
+        db.execSQL("DROP TABLE IF EXISTS usuarios");
+    }
+}
--- app/src/main/java/cl/santotomas/formulario/MainActivity.java
+++ app/src/main/java/cl/santotomas/formulario/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd usuarios",null,1);
     }
 }
